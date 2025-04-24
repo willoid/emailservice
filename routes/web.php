863b758b16ba6366163804853eb7d5aee68eebcf
willy <willoid.webdev@gmail.com>
2025-04-24 12:38:55 +0200

    updated routes

--- routes/web.php
+++ routes/web.php
@@ -5,3 +5,6 @@ use Illuminate\Support\Facades\Route;
 Route::get('/', function () {
     return view('welcome');
 });
+Route::get('history', [App\Http\Controllers\EmailController::class, 'history'])->name('history');
+Route::delete('message/{id}', [App\Http\Controllers\EmailController::class, 'deleteMessage'])->name('message.delete');
+Route:get('message/{id}', [App\Http\Controllers\EmailController::class, 'showMessage'])->name('message.show');
