9e0baaf6d8650e8f1e2a07065c9a60797fc070f1
willy <willoid.webdev@gmail.com>
2025-04-24 12:40:04 +0200

    corrected bug in routes

--- routes/web.php
+++ routes/web.php
@@ -7,4 +7,4 @@ Route::get('/', function () {
 });
 Route::get('history', [App\Http\Controllers\EmailController::class, 'history'])->name('history');
 Route::delete('message/{id}', [App\Http\Controllers\EmailController::class, 'deleteMessage'])->name('message.delete');
-Route:get('message/{id}', [App\Http\Controllers\EmailController::class, 'showMessage'])->name('message.show');
+Route::get('message/{id}', [App\Http\Controllers\EmailController::class, 'showMessage'])->name('message.show');
