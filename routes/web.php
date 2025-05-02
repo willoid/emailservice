59e78e9eafd264ae94b24c1a4a3fd80c86a39181
willy <willoid.webdev@gmail.com>
2025-05-02 16:37:02 +0200

    created formular and route to send emails

--- routes/web.php
+++ routes/web.php
@@ -1,10 +1,12 @@
 <?php
 
 use Illuminate\Support\Facades\Route;
+use App\Http\Controllers\EmailController;
 
 Route::get('/', function () {
     return view('welcome');
 });
-Route::get('history', [App\Http\Controllers\EmailController::class, 'history'])->name('history');
+Route::post('/send-email', [EmailController::class, 'sendMail'])->name('send.email');
+Route::get('history', [App\Http\Controllers\EmailController::class, 'history'])->name('message.history');
 Route::delete('message/{id}', [App\Http\Controllers\EmailController::class, 'deleteMessage'])->name('message.delete');
 Route::get('message/{id}', [App\Http\Controllers\EmailController::class, 'showMessage'])->name('message.show');
